4b14eed8c971b8527d66fa6403560d30ed50ee25
Dimitar Atanasov <datanasov@proofpoint.com>
2016-01-26 20:08:47 +0200

    initial commit

---
+++ js/api.js
@@ -0,0 +1,72 @@
+(function(){
+    "use strict";
+
+    var currnetMap = {};
+
+    function loadMap(map) {
+        currnetMap = map;
+    }
+
+    function addConcept(label, posx, posy) {
+        currnetMap.concepts.push({
+           "label":label,
+           "posx":posx,
+           "posy":posy
+        });
+    }
+
+    function addRelationship(label, source, target) {
+        currnetMap.relationships.push({
+            "label":label,
+            "source":source,
+            "target":target
+            });
+    }
+
+    function updateConcept(id, label, posx, posy) {
+        //code
+    }
+
+    function updateRelationshp(id, label, posx, posy) {
+        //code
+    }    
+
+    function deleteConcept(id) {
+        //code
+    }
+
+    function deleteRelationship(id) {
+        //code
+    }
+
+    function createMap(title) {
+        var requestResponse;
+        YUI().use('io-base',function(Y){
+            var uri = "maps",
+             json = {"title":title},
+             cfg = {
+                method: 'POST',
+                data: Y.JSON.stringify(jData),
+                 headers: {
+                    'Content-Type': 'application/json',
+                 },
+                 on:{
+                    
+                    success: function (id, response) {
+                     requestResponse =  response.responseText; 
+                    }
+                 }
+                };
+            });
+        //do something with response
+    }
+
+    function saveMap(id) {
+       console.log(id);
+    }
+
+    window.Map = {
+        save: saveMap
+    };
+
+}());
