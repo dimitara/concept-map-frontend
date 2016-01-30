90d2639cb791ec6afaa034501efcc5a53746641a
Dimitar Atanasov <dimitara@gmail.com>
2016-01-31 01:32:22 +0200

    correct drag and drop of the node & line

--- js/app-new.js
+++ js/app-new.js
@@ -2,39 +2,44 @@ var s = Snap('#snap');
 var nodes = [];
 var rels = [];
 
+var nodeDefaultX1 = 100;
+var nodeDefaultY1 = 100;
+var nodeDefaultX2 = 100;
+var nodeDefaultY2 = 50;
+var rFactor = 5;
+
+
 function addNode(){
     var group = s.group();
     group.attr('height', 50);
     group.attr('width', 100);
-    group.attr('x', 100);
-    group.attr('y', 100);
+    group.attr('x', 0);
+    group.attr('y', 0);
     group.rels = [];
 
-    var node = s.rect(100, 100, 100, 50, 5, 5);
+    var node = s.rect(nodeDefaultX1, nodeDefaultY1, nodeDefaultX2, nodeDefaultY2, rFactor, rFactor);
     node.attr('stroke', '#000');
     node.attr('fill', '#fff');
-    var nodeText = s.text(100, 100+25, 'Concept Node');
+    var nodeText = s.text(nodeDefaultX1 + 3, nodeDefaultY1+nodeDefaultY2/1.7, 'Concept Node');
     nodeText.attr('fill', '#000');
     
     group.add(node);
     group.add(nodeText);
 
     nodeText.click(function(){
-        var newLabel = prompt('', this.attr('text'));
-        this.attr({text: newLabel});
-        node.attr('width', this.getBBox().width + 20);
-
-        node.parent().attr('width', this.getBBox().width + 20);
+        this.attr({text: 'Concept Node'});
+        //node.attr('width', this.getBBox().width + 20);
+        //node.parent().attr('width', this.getBBox().width + 20);
 
         //todo: reposition the text inside the rectangle
     });
 
     node.drag(moveNode,
         function() {
-            console.log("Move started");
+            
         },
         function() {
-            console.log("Move stopped");
+            moveOutNode.call(this);
         }
     );
 
@@ -44,6 +49,12 @@ function addNode(){
 function addRelation(){
     var group = s.group();
     var line = s.line(100, 80, 200, 80);
+
+    group.x1 = 100;
+    group.y1 = 80;
+    group.x2 = 200;
+    group.y2 = 80;
+
     var circleOne = s.circle(100, 80, 3);
     circleOne.attr('data-item', 'one');
 
@@ -53,6 +64,7 @@ function addRelation(){
     line.attr('stroke', '#000');
     group.add(line);
     group.add(circleOne);
+    group.add(circleTwo);
 
     circleOne.drag(moveLine, function(){}, function(){
         moveOutLine.call(this, parseFloat(line.attr('x1')), parseFloat(line.attr('y1')), 'one', group);
@@ -63,9 +75,16 @@ function addRelation(){
 }
 
 var moveNode = function(dx, dy, posx, posy) {
-    this.parent().attr('x', dx);
-    this.parent().attr('y', dy);
-    this.parent().attr('transform', 'translate(' + dx + ',' + dy + ')');
+    var x = parseFloat(this.parent().attr('x'));
+    var y = parseFloat(this.parent().attr('y'));
+
+    x += dx;
+    y += dy;
+
+    this.parent().tempX = x;
+    this.parent().tempY = y;
+
+    this.parent().attr('transform', 'translate(' + x + ',' + y + ')');
 
     if(this.parent().rels){
         var rels = this.parent().rels;
@@ -83,7 +102,17 @@ var moveNode = function(dx, dy, posx, posy) {
     }
 }
 
+var moveOutNode = function(){
+    this.parent().attr('x', this.parent().tempX);
+    this.parent().attr('y', this.parent().tempY);
+};
+
 var moveOutLine = function(posx, posy, type, el){
+    this.parent().x1 = this.parent().select('line').attr('x1');
+    this.parent().y1 = this.parent().select('line').attr('y1');
+    this.parent().x2 = this.parent().select('line').attr('x2');
+    this.parent().y2 = this.parent().select('line').attr('y2');
+
     var resultNode = selectNode(posx, posy);
 
     if(resultNode){
@@ -97,22 +126,27 @@ var moveOutLine = function(posx, posy, type, el){
 var moveLine = function(dx, dy, posx, posy){
     var end = this.attr('data-item');
     var line = this.parent().select('line');
+    console.log(this);
+    var x1 = parseFloat(this.parent().x1);
+    var y1 = parseFloat(this.parent().y1);
+    var x2 = parseFloat(this.parent().x2);
+    var y2 = parseFloat(this.parent().y2);
 
-    selectNode(posx, posy);
+
+    //selectNode(posx, posy);
 
     if(end === 'one'){
-        line.attr('x1', posx);
-        line.attr('y1', posy);
-        this.attr('cx', posx);
-        this.attr('cy', posy);
+        line.attr('x1', x1 + dx);
+        line.attr('y1', y1 + dy);
+        this.attr('cx', x1 + dx);
+        this.attr('cy', y1 + dy);
     }
 
     if(end === 'two'){
-        line.attr('x2', posx);
-        line.attr('y2', posy);
-
-        this.attr('cx', posx);
-        this.attr('cy', posy);
+        line.attr('x2', x2 + dx);
+        line.attr('y2', y2 + dy);
+        this.attr('cx', x2 + dx);
+        this.attr('cy', y2 + dy);
     }
 }
 
